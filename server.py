6d6a68895a9ffec62e7e687bd9d957528d5d4cba
Dajka Kristof <kmintkristof@gmail.com>
2019-02-06 13:03:38 +0100

    added home

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-import POST as POST
+
 from flask import Flask, render_template, redirect, request, url_for, make_response
 import util
 import data_manager
@@ -10,12 +10,12 @@ app = Flask(__name__)
 @app.route('/list')
 def home():
     headers = ["Date", "Title", "Question", "Show"]
-    table = {"SFG": "2018-10-11 13:12", "Title", "Question", "Watch"}
+    table = {"SFG", "2018-10-11 13:12", "Title", "Question", "Watch"}
+
     return render_template("home.html", table=table, headers=headers)
 
 
-
-@app.route('/add-question', method=['GET', 'POST'])
+@app.route('/add-question', methods=['GET', 'POST'])
 def add_question():
     pass
 
@@ -27,7 +27,7 @@ def display_question(id):
 def give_answer(id):
     pass
 
-if __name__ = "__main__":
+if __name__ == "__main__":
     app.run(
         host='0.0.0.0',
         port=6000,
