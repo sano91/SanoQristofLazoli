09c3f3f01dcd4328741af8fdfc7c7dabcb5186d1
lazoli95 <zoltan.lazar1995@gmail.com>
2019-02-07 15:10:07 +0100

    stylesheet

--- data_manager.py
+++ data_manager.py
@@ -9,13 +9,13 @@ def open_question(csv_file):
 
         for lines in opened_file:
             print(lines)
-
+'''
 def write_to_csv(csv_file, data):
     with open(csv_file, "a") as f:
         csv.DictWriter(f, data)
-    return None
+    return None'''
 
-'''
+
 def get_data_from_csv(csv_file="sample_data/question.csv", id=None):
     with open(csv_file, "r") as f:
         reader = csv.DictReader(f)
