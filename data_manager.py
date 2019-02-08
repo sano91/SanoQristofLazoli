708fedddc27d025a28e1b66fd453643870dcada0
Kulin Sandor <vitaquant@gmail.com>
2019-02-08 09:12:22 +0100

    answer

--- data_manager.py
+++ data_manager.py
@@ -16,6 +16,8 @@ def write_to_csv(csv_file, data):
     return None
 
 '''
+
+
 def get_data_from_csv(csv_file="sample_data/question.csv", id=None):
     with open(csv_file, "r") as f:
         reader = csv.DictReader(f)
@@ -23,6 +25,7 @@ def get_data_from_csv(csv_file="sample_data/question.csv", id=None):
 
         for data in reader:
             item = dict(data)
+
             if id is not None and id == item["id"]:
                 return item
             table.append(item)
@@ -43,8 +46,35 @@ def get_id_by_title(csv_file, title):
             return line['id']
 
 
+answer_id = 2
 
+
+def create_right_format(message, type, question_id):
+    global answer_id
+    submission_time = 9876523
+    answer_id += 1
+    if type == 'answer':
+        answer_row = {"id": answers, "submission_time": submission_time, "vote_number": 0, "question_id": question_id,
+                      "message": message, "image": 0}
+        return answer_row
+
+
+def write_to_the_file(data, file, type):
+    if type == 'question':
+        headers_q = ['id', 'submission_time', 'view_number', 'vote_number', 'title', 'message', 'image']
+        with open(file, 'a') as questions:
+            newquestion = csv.DictWriter(questions, fieldnames=headers_q)
+            newquestion.writerow(data)
+    elif type == 'answer':
+        headers_a = ['id', 'submission_time', 'vote_number', 'question_id', 'message', 'image']
+        with open(file, 'a') as questions:
+            newquestion = csv.DictWriter(questions, fieldnames=headers_a)
+            newquestion.writerow(data)
+
+
+answers = get_data_from_csv(csv_file="sample_data/answer.csv", id="1")
+print(answers)
+for key in answers.keys():
+    print(answers[key])
 
 print(get_id_by_title("sample_data/question.csv", "How to make lists in Python?"))
-
-
