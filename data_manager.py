45771c64efe8aa20a28a2b062f314494d71dccfc
Dajka Kristof <kmintkristof@gmail.com>
2019-02-19 15:37:40 +0100

    csv to SQL

--- data_manager.py
+++ data_manager.py
@@ -1,78 +1,32 @@
-import csv
+import connection
 
-'''
-def open_question(csv_file):
-    with open(csv_file, "r") as f:
-        opened_file = f.readlines()
-        table = [element.replace("\n", "").split(",") for element in opened_file]
+@connection.connection_handler
+def get_answers(cursor, city):
+    cursor.execute("""
+                    SELECT * FROM answer;
+                   """)
+    answers = cursor.fetchall()
 
-    return table
-
-
-
-def write_to_csv(csv_file, data):
-    with open(csv_file, "a") as f:
-        csv.DictWriter(f, data)
-    return None
-
-'''
-def get_data_from_csv(csv_file="sample_data/question.csv", id=None):
-    with open(csv_file, "r") as f:
-        reader = csv.DictReader(f)
-        table = []
-        selected_question = []
-        for data in reader:
-            item = dict(data)
-            if id is not None and id == item["id"]:
-                return item
-            table.append(item)
-
-        return table
+    return answers
 
 
-def convert_time(time):
-    normal_time = datetime.fromtimestamp(time)
-    return normal_time
+@connection.connection_handler
+def get_questions(cursor):
+    cursor.execute("""
+                    SELECT * FROM question;
+                   """)
+    questions = cursor.fetchall()
 
+    return questions
 
-def get_headers(csv_file):
-    with open(csv_file, "r") as file:
-        headers = file.readline()
-        header = headers.split(",")
-    return header
+@connection.connection_handler
+def new_question(cursor, title, message, image):
+    cursor.execute('''
+    INSERT INTO question (view_number, vote_number, title, message, image)
+    VALUES (%(view_number)s, %(vote_number)s, %(title)s, %(message)s,%(image)s);''',
+    {'view_number':0, 'vote_number':0, 'title':title, 'message':message, 'image':image})
 
+# SQL generates own ID
 
-def get_id_by_title(csv_file, title):
-    table = get_data_from_csv(csv_file, id=None)
-    for line in table:
-        if line['title'] == title:
-            return line['id']
-
-
-answer_id = 2
-
-
-def create_right_format(message, type, question_id):
-    global answer_id
-    submission_time = 9876523
-    answer_id += 1
-    if type == 'answer':
-        answer_row = {"id": answers, "submission_time": submission_time, "vote_number": 0, "question_id": question_id,
-                      "message": message, "image": 0}
-        return answer_row
-
-
-def write_to_the_file(data, file, type):
-    if type == 'question':
-        headers_q = ['id', 'submission_time', 'view_number', 'vote_number', 'title', 'message', 'image']
-        with open(file, 'a') as questions:
-            newquestion = csv.DictWriter(questions, fieldnames=headers_q)
-            newquestion.writerow(data)
-    elif type == 'answer':
-        headers_a = ['id', 'submission_time', 'vote_number', 'question_id', 'message', 'image']
-        with open(file, 'a') as questions:
-            newquestion = csv.DictWriter(questions, fieldnames=headers_a)
-            newquestion.writerow(data)
-
-
+    return cursor
 
--- server.py
+++ server.py
@@ -9,52 +9,22 @@ app = Flask(__name__)
 
 generated_ids = []
 
+
 @app.route('/')
-@app.route('/list')
-def home():
-    header = data_manager.get_headers("sample_data/question.csv")
-    header = header[1:5]
-    table = data_manager.get_data_from_csv("sample_data/question.csv", id=None)
-    return render_template("home.html", questions=table, headers=header)
+def home_page():
+    all_questions = data_manager.get_questions()
 
+    return render_template('home.html', questions=all_questions)
 
-@app.route('/add-question', methods=['GET', 'POST'])
+@app.route('/add-question', METHODS='POST')
 def add_question():
-    headers = ['id', 'submission_time', 'view_number', 'vote_number', 'title', 'message', 'image']
-    if request.method == 'POST':
-        with open('sample_data/question.csv', 'a') as questions:
-            newquestion = csv.DictWriter(questions, fieldnames=headers)
-            newquestion.writerow(
-                {'id': util.generate_random(generated_ids), 'submission_time': 'Couldnt do',
-                 'view_number': '0', 'vote_number': '0',
-                 'title': request.form['title'],
-                 'message': request.form['message']})
+    message = request.form.get('message')
+    title = request.form.get('title')
+    image = request.form.get('image')
+    data_manager.new_question(title, message, image)
 
-        return redirect('/')
+    return render_template('add-question.html', message=message, title=title, image=image)
 
-    return render_template('add-question.html')
-
-
-@app.route("/question/<question_id>")
-def display_question(question_id):
-    my_data = data_manager.get_data_from_csv(csv_file="sample_data/question.csv", id=question_id)
-    question_header = data_manager.get_headers("sample_data/question.csv")
-    answers = data_manager.get_data_from_csv(csv_file="sample_data/answer.csv", id=question_id)
-    answers_headers = data_manager.get_headers("sample_data/answer.csv")
-
-    return render_template('q-and-a.html', question=my_data, questionheader=question_header, answer=answers,
-                           answerheader=answers_headers)
-
-
-@app.route("/question/<id>/new-answer", methods=["GET", "POST"])
-def give_answer(id):
-    if request.method == "POST":
-
-        answer_message = request.form["message"]
-        new_answer = data_manager.create_right_format(id, answer_message, "answer")
-        data_manager.write_to_the_file(new_answer, "sample_data/answer.csv", "answer")
-        return redirect("/")
-    return render_template("answer.html")
 
 
 if __name__ == "__main__":
